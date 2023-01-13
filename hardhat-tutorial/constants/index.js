427c0e2f5c8c8a100fdf5a306dccd17a038471ed
sah_314 <khadayatesahil@gmail.com>
2023-01-13 10:22:17 +0530

    Completed ico code

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS ="0xC4680fa016D96bCB7F4e0E9fF1C57e970d75D2AE";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
